8f7ab6aafac1fe627a23dcfa883ea120a56105de
saad <saad.aslam39@gmail.com>
2019-02-19 23:46:03 -0500

    * Added function level documentation for `get_indicator`, `get_url`, and `set_api_key`. * Modified `DESCRIPTION` so that it's somewhat more informative * Added a license

--- R/get_indicator.R
+++ R/get_indicator.R
@@ -1,13 +1,44 @@
-get_indicator <-
-  function(category = c("economic", "housing"),
-           indicator) {
-    if (category == "housing")
-      catg_path <- "housing-indicators"
-    else if (category == "economic")
-      catg_path <- "economic-forecasts"
+#' Get economic or housing indicators
+#'
+#' @param category Either "economic" or "housing"
+#' @param indicator One of the indicator series available under housing or economic indicators. See 'Details' for options.
+#'
+#' @return A `grouped_df` that is grouped by the column `report_date`.
+#'
+#' @details
+#'
+#' This function has access to the following indicator series for housing specific data:
+#'
+#' * gross-domestic-product
+#' * personal-consumption-expenditures
+#' * residential-fixed-investment
+#' * business-fixed-investment
+#' * government-consumption-and-investment
+#' * net-exports
+#' * change-in-business-inventories
+#' * gdp-price-index
+#' * consumer-price-index
+#' * consumer-price-index-excl-food-and-energy
+#' * personal-chain-expenditures-chain-price-index
+#' * personal-chain-expenditures-chain-price-index-excl-food-and-energy
+#' * unemployment-rate-percent
+#' * employment-total-nonfarm-percent-change-quarterly-saar-annual-q4-q4
+#' * federal-funds-rate
+#' * 1-year-treasury-note-yield
+#' * 10-year-treasury-bond-yield
+#'
+#' @export
+#'
+#' @examples
+#' get_indicator("economic", "gross-domestic-product")
+#' get_indicator("housing", "housing-starts-single-family")
+#' @seealso
+#' [get_url()]
+get_indicator <- function(category = c("economic", "housing"), indicator) {
+    if (category == "housing") catg_path <- "housing-indicators"
+    else if (category == "economic") catg_path <- "economic-forecasts"
 
-    indicator_path <-
-      glue::glue("/v1/{catg_path}/indicators/{indicator}")
+    indicator_path <- glue::glue("/v1/{catg_path}/indicators/{indicator}")
 
     url_string <- httr::modify_url(api_url, path = indicator_path)
 
@@ -18,8 +49,7 @@ get_indicator <-
 
     tbl <- dplyr::tibble(l1 = time_series)
 
-    tbl <- dplyr::mutate(tbl,
-                         name = purrr::map(l1, names))
+    tbl <- dplyr::mutate(tbl, name = purrr::map(l1, names))
 
     tbl <- tidyr::unnest(tbl, l1, name, .id = "id")
 
@@ -27,13 +57,11 @@ get_indicator <-
 
     tbl <- janitor::clean_names(tbl, "snake")
 
-    tbl <-
-      tidyr::unnest(tbl, effective_date, indicator_name, .id = "id3")
+    tbl <- tidyr::unnest(tbl, effective_date, indicator_name, .id = "id3")
 
     tbl <- tidyr::unnest(tbl, points, .id = "id4")
 
-    tbl <- dplyr::mutate(tbl,
-                         point_names = purrr::map(points, ~ names(.x)))
+    tbl <- dplyr::mutate(tbl, point_names = purrr::map(points, ~names(.x)))
 
     tbl <- tidyr::unnest(tbl, points, point_names, .id = "id5")
 
@@ -41,26 +69,29 @@ get_indicator <-
 
     tbl <- tidyr::unnest(tbl, forecast, quarter, value, year)
 
-    tbl <- dplyr::mutate(
-      tbl,
-      value = as.numeric(value),
-      effective_date = as.Date(effective_date),
-      quarter_num = substring(quarter, 2),
-      month = dplyr::case_when(
-        quarter_num == "1" ~ "Feb",
-        quarter_num == "2" ~ "May",
-        quarter_num == "3" ~ "Aug",
-        quarter_num == "4" ~ "Nov",
-        TRUE ~ "EOY"
-      ),
-      date = dplyr::if_else(
-        month != "EOY",
-        paste0(as.character(year), month, "15"),
-        paste0(as.character(year), "Dec", "31")
-      ),
-      indicator_val_dt = as.Date(date, format = "%Y%b%d"),
-      eoy_flag = dplyr::if_else(month == "EOY", TRUE, FALSE)
-    )
+    tbl <-
+      dplyr::mutate(
+        tbl,
+        value = as.numeric(value),
+        effective_date = as.Date(effective_date),
+        quarter_num = substring(quarter, 2),
+        month = dplyr::case_when(
+          quarter_num == "1" ~ "Feb",
+          quarter_num == "2" ~ "May",
+          quarter_num == "3" ~ "Aug",
+          quarter_num == "4" ~
+            "Nov",
+          TRUE ~ "EOY"
+        ),
+        date = dplyr::if_else(
+          month != "EOY",
+          paste0(as.character(year), month, "15"),
+          paste0(as.character(year),
+                 "Dec", "31")
+        ),
+        indicator_val_dt = as.Date(date, format = "%Y%b%d"),
+        eoy_flag = dplyr::if_else(month == "EOY", TRUE, FALSE)
+      )
 
     result <-
       dplyr::select(tbl,
@@ -77,4 +108,4 @@ get_indicator <-
 
     result
 
-  }
+}
--- R/get_url.R
+++ R/get_url.R
@@ -1,46 +1,36 @@
+#' Call a particular URL from Fannie Mae's public API.
+#'
+#' @param url The URL. This should start with "https://api.theexchange.fanniemae.com/".
+#'
+#' @return A list with the following elements:
+#' * content -- The parsed JSON returned from API call.
+#'   Note that the object is returned as a nested list.
+#' * url -- The URL used to make the call
+#' * response -- The response object created by `httr::GET`
+#' @export
+#'
+#' @examples
+#' get_url("https://api.theexchange.fanniemae.com/v1/mortgage-lender-sentiment/results")
 get_url <- function(url) {
 
-  if(Sys.getenv("fannieapi_key") == "") {
-    stop("Please set an API key with `fannieapi::set_api_key()`", call. = FALSE)
-  }
+    if (Sys.getenv("fannieapi_key") == "") {
+        stop("Please set an API key with `fannieapi::set_api_key()`", call. = FALSE)
+    }
 
-  resp <- httr::GET(
-    url,
-    httr::add_headers(Authorization = Sys.getenv("fannieapi_key"), accept = "application/json")
-  )
+    resp <- httr::GET(url, httr::add_headers(Authorization = Sys.getenv("fannieapi_key"), accept = "application/json"))
 
-  if(httr::http_type(resp) != "application/json") {
-    stop("API did not return JSON", call. = FALSE)
-  }
+    if (httr::http_type(resp) != "application/json") {
+        stop("API did not return JSON", call. = FALSE)
+    }
 
-  parsed <- jsonlite::fromJSON(
-    suppressMessages(httr::content(resp, "text")),
-    simplifyVector = FALSE
-  )
+    parsed <- jsonlite::fromJSON(suppressMessages(httr::content(resp, "text")), simplifyVector = FALSE)
 
-  if (httr::http_error(resp)) {
-    stop(
-      sprintf(
-        "API request failed [%s]\n%s\nSee https://developer.theexchange.fanniemae.com/assets/pdf/FAQ.pdf for details.",
-        httr::status_code(resp),
-        parsed$message
-      ),
-      call. = FALSE
-    )
-  }
+    if (httr::http_error(resp)) {
+        stop(sprintf("API request failed [%s]\n
+                     %s\n
+                     See https://developer.theexchange.fanniemae.com/assets/pdf/FAQ.pdf for details.",
+            httr::status_code(resp), parsed$message), call. = FALSE)
+    }
 
-  list(
-    content = parsed,
-    url = url,
-    response = resp
-  )
+    list(content = parsed, url = url, response = resp)
 }
-
-
-# print.fannie_api <- function(x, ...) {
-#   cat("<fannie_api", x$path, ">\n", sep = "")
-#   cat("<", x$url, ">\n", sep="")
-#   str(x$content, max.level = 1)
-#   invisible(x)
-# }
-
